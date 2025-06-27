f31c563d1019a19d3a8aef2184ea7f5760907235
xuchengyu20130704 <xcy20130704@outlook.com>
2025-06-27 09:28:59 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -97,7 +97,28 @@ function loadCSVToTable(url, containerId) {
         orderCellsTop: true,
         fixedHeader: true,
         language: {
-          url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-CN.json"
+          "sProcessing": "处理中...",
+          "sLengthMenu": "每页显示 _MENU_ 条记录",
+          "sZeroRecords": "没有匹配结果",
+          "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+          "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+          "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+          "sInfoPostFix": "",
+          "sSearch": "搜索:",
+          "sUrl": "",
+          "sEmptyTable": "表中数据为空",
+          "sLoadingRecords": "载入中...",
+          "sInfoThousands": ",",
+          "oPaginate": {
+            "sFirst": "首页",
+            "sPrevious": "上一页",
+            "sNext": "下一页",
+            "sLast": "末页"
+          },
+          "oAria": {
+            "sSortAscending": ": 以升序排列此列",
+            "sSortDescending": ": 以降序排列此列"
+          }
         }
       });
 
